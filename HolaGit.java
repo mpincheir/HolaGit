54b29f30fee0f7ff86a0f99187ddbe9392246dc9
Miguel Pincheira <miguel.pincheira@gmail.com>
2016-03-29 12:32:24 -0300

    Creacion del helper

--- HolaGit.java
+++ HolaGit.java
@@ -1,8 +1,13 @@
 public class HolaGit {
 
 	public static void main (String args[]) {
+		Helper helper = new Helper();		
 		System.out.println("Hola Git");
+		helper.llamar();
 		System.out.println("Soy parte del segundo commit");
+		helper.llamar();
 		System.out.println("Soy parte del tercer commit");
+		helper.llamar();
+		System.out.println("Total Llamados " + helper.getLlamados() );
 	}
 }
